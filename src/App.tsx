a1c2bed8ead0d50fe13dcffd752cb7397db091aa
xuntianxuntian <871966006@qq.com>
2025-08-03 00:15:16 +0800

    rebuld app and finish dark mode setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from 'react'
+import { themeChange } from 'theme-change'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    themeChange(false)
+    // 👆 false parameter is required for react project
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='m-4 flex flex-col-3 gap-6'>
+        <button className="btn btn-outline" data-set-theme="light">
+          light
+        </button >
+        <button className="btn btn-outline" data-set-theme="dracula">
+          dracula
+        </button >
+        <button className="btn btn-outline" data-set-theme="garden">
+          garden
+        </button >
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='m-4'>
+        <button className="btn btn-info">
+          Info
+        </button >
+        <button className="btn btn-error" >
+          Error
+        </button >
+        <button className="btn btn-success" >
+          Sucess
+        </button >
+        <button className="btn btn-disabled">
+          Disable
+        </button >
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }

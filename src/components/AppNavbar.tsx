52cdb4a46267aa5dcb14156fa1b5010bd7e14dbf
xuntianxuntian <871966006@qq.com>
2025-08-03 22:21:17 +0800

    update some features

---
+++ src/components/AppNavbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppNavbar = () => {
+  return (
+    <nav>AppNavbar</nav>
+  )
+}
+
+export default AppNavbar
---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppSidebar = () => {
+  return (
+    <div className=''>AppSidebar</div>
+  )
+}
+
+export default AppSidebar
